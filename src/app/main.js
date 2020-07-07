9b6d344af326ec3a6b3187054deeb61cabad9226
钟凯华 <493014828@qq.com>
2020-07-07 17:09:31 +0800

    app main.js commit

---
+++ src/app/main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+let mainWin = null;
+
+let createWindow = () => {
+    mainWin = new BrowserWindow({
+        width: 960,
+        height: 640,
+        minHeight: 640,
+        minWidth: 960,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    mainWin.loadFile(path.join(__dirname, '/index.html'));
+    app.on('close', () => {
+        app.quit();
+    })
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-close', () => {
+    if (process.platform !== 'drawin') {
+        app.quit();
+    }
+});
